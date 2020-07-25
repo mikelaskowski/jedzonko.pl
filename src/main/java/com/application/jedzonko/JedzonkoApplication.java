6c540898918181dc989503177e5311ca6f615221
ka5ta <kasta.urbanska@gmail.com>
2020-07-25 19:12:57 +0200

    Create Barcode controller for PNG display in website and pdf to save file on local drive. Registering HttpMessageConverter to generate PNG from BufferedImage.

--- src/main/java/com/application/jedzonko/JedzonkoApplication.java
+++ src/main/java/com/application/jedzonko/JedzonkoApplication.java
@@ -1,19 +1,28 @@
 package com.application.jedzonko;
 
+import com.application.jedzonko.SavingOrdersToPDF.PdfCreator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.awt.image.BufferedImage;
 
 @SpringBootApplication
 public class JedzonkoApplication {
-    @Autowired
-    private ObjectMapper objectMapper;
+
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(JedzonkoApplication.class, args);
+    }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+
     }
 
 
---
+++ src/main/java/com/application/jedzonko/SavingOrdersToPDF/QRCodeGenerator.java
@@ -0,0 +1,30 @@
+package com.application.jedzonko.SavingOrdersToPDF;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+public class QRCodeGenerator {
+
+    public static BufferedImage generateQRCodeBufferedImage(String barcodeText) throws Exception {
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
+
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+    public static byte[] generateQRCodePNG(String barcodeText) throws Exception {
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
+
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "png", bao);
+        bao.close();
+
+        return bao.toByteArray();
+    }
+}
