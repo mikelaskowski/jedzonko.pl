d1288c16fa0b027b96495fcae1bb142ce6fa65ef
Michal Laskowski <mikelaskowski@o2.pl>
2020-07-25 17:07:22 +0200

    add dependencies and create config class for Swagger

---
+++ src/main/java/com/java25wro/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.java25wro.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    //localhost:8080/swagger-ui.html  --> endpoints
+}
