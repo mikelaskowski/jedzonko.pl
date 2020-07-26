558c37523dd05902ac58a72e3c697f11b1e0bbd1
Michal Laskowski <mikelaskowski@o2.pl>
2020-07-26 14:24:59 +0200

    add Port to Procfile

---
+++ src/main/java/com/java25wro/controller/EmptyController.java
@@ -0,0 +1,13 @@
+package com.java25wro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmptyController {
+
+    @GetMapping(value = "/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
