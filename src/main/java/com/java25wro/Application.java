c0f310874f41e4c7098a91926f9eb9e0891acec2
Michal Laskowski <mikelaskowski@o2.pl>
2020-07-26 15:28:58 +0200

    fix CrossOrigin

--- src/main/java/com/java25wro/Application.java
+++ src/main/java/com/java25wro/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "*" )
+@CrossOrigin("*")
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
 
