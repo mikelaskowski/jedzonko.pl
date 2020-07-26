ccd74ef5168cf12889e7a3bfe1c77e87770cad5b
Michal Laskowski <mikelaskowski@o2.pl>
2020-07-26 15:39:23 +0200

    add CrossOrigin annotation to all Controllers

--- src/main/java/com/java25wro/controller/SurveyController.java
+++ src/main/java/com/java25wro/controller/SurveyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin(value = "*")
 @RequestMapping(value = "/survey")
 public class SurveyController {
 
