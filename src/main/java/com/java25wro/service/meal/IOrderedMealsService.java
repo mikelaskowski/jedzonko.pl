2555ef0a0886d350058b50f7577c207f23a40c3a
ka5ta <kasta.urbanska@gmail.com>
2020-07-27 23:16:54 +0200

    Renamed interface for OrderService to interface OrderedMealsService, Created OrderedMealsService with method findAllMealsByOrderID

--- src/main/java/com/java25wro/service/meal/IOrderedMealsService.java
+++ src/main/java/com/java25wro/service/meal/IOrderedMealsService.java
@@ -1,9 +1,9 @@
-package com.java25wro.service.order;
+package com.java25wro.service.meal;
 
 import com.java25wro.model.OrderedMeals;
 
 import java.util.Set;
 
-public interface IOrderService {
+public interface IOrderedMealsService {
     Set<OrderedMeals> findAllMealsByOrderId(Long orderId);
 }
