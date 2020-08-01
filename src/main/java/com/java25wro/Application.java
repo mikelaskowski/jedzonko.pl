950ddf8b00512f8faff7d345398471b1ee2142f5
ka5ta <kasta.urbanska@gmail.com>
2020-08-01 17:22:30 +0200

    Adding Bean for PNG image of QR code from JedzonkoApplication.java

--- src/main/java/com/java25wro/Application.java
+++ src/main/java/com/java25wro/Application.java
@@ -2,11 +2,22 @@ package com.java25wro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.awt.image.BufferedImage;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+
     }
 }
