864a34e7c45cd0a0c855a6a82563e957a842eab8
Patrycja1986 <55314242+Patrycja1986@users.noreply.github.com>
2020-08-06 11:27:15 +0200

    added corsConfigurer and new dependency httpClient

--- src/main/java/com/java25wro/Application.java
+++ src/main/java/com/java25wro/Application.java
@@ -2,11 +2,24 @@ package com.java25wro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("HEAD","POST","GET","PUT","PATCH","DELETE");
+            }
+        };
     }
 }
